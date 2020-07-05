557bbb5363c4587ec14729bd2767fc0a99a2f626
pepeyen <erickfcaldeira@gmail.com>
2020-07-05 13:02:28 -0300

    Updates

---
+++ precache-manifest.d312c76109ce583d52f24b35997c6255.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3661ca0de4bc23fc0a8af52eeac6d64f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8566f73c27ad3f55422e",
+    "url": "/portfolio/static/css/main.c15044c1.chunk.css"
+  },
+  {
+    "revision": "015bdc007ac8a805d3f6",
+    "url": "/portfolio/static/js/2.fa65585f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.fa65585f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8566f73c27ad3f55422e",
+    "url": "/portfolio/static/js/main.29db3563.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/portfolio/static/media/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "277289c53af7cb469c1dc5dca3adca35",
+    "url": "/portfolio/static/media/Ubuntu-Light.277289c5.ttf"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/portfolio/static/media/Ubuntu-Medium.8e22c2a6.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/portfolio/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "46f0a3ad815962005753c02c4181aadc",
+    "url": "/portfolio/static/media/UbuntuMono-Bold.46f0a3ad.ttf"
+  },
+  {
+    "revision": "e097b71641f2524e09820d9122b7e910",
+    "url": "/portfolio/static/media/UbuntuMono-Regular.e097b716.ttf"
+  }
+]);
