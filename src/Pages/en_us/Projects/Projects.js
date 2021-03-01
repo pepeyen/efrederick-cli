44227ddd8d6801db934cf3a289776b90c0c34d0e
pepeyen <erickfcaldeira@gmail.com>
2021-02-28 23:29:53 -0300

    Introduced API based projectList

--- src/Pages/en_us/Projects/Projects.js
+++ src/Pages/en_us/Projects/Projects.js
@@ -1,50 +1,65 @@
-import React from 'react';
+import React, {
+	useEffect,
+	useState
+} from 'react';
 import '../../css/index.scss';
 import '../../css/projects.scss';
 
 function Projects() {
-	const projectList = [{projectName: 'pyramid-3d'}, {projectName: 'oop-tech'}, 
-	{projectName: 'cs-tech', projectLiveURL: 'https://pepeyen.github.io/cs-tech'}, {projectName: 'efrederick-cli', projectLiveURL: 'this'}, 
-	{projectName: 'efrederick', projectLiveURL: 'https://efrederick.dev'}, {projectName: 'namah-front-end', projectLiveURL: 'https://pepeyen.github.io/namah-front-end'}, 
-	{projectName: 'namah-back-end', projectLiveURL: 'https://project-namah.herokuapp.com'}];
+	let [projectList, setProjectList] = useState([]);
+
+	useEffect(() => {
+		fetch(`${process.env.REACT_APP_BACK_END_HOST}/efrederick/api/v1/projects`,{
+			method: 'GET'
+		})
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			setProjectList(data.projects.list);
+		});
+	},[]);
 
     return (
       <React.Fragment> 
         <p className = "cli__info">Running: <strong className = "cli__source"> Projects.sh</strong></p><br></br>
         <ul className = "cli__output-text">
 			{
-				projectList.map((element, index) => {
-					if(element.projectLiveURL){
-						if(element.projectLiveURL === 'this'){
-							return(
-								<li
-									key={index}
-									className = "cli__project"
-								>
-									{element.projectName} <a className = "cli__link" href={`https://github.com/pepeyen/${element.projectName}`} target="_blank" rel="noopener noreferrer">&lt;repo&gt;</a>  &lt;this webapp&gt;
-								</li>
-							);
+				projectList && projectList.length > 0 ?
+					projectList.map((element, index) => {
+						if(element.repositoryURL){
+							if(element.repositoryName === 'efrederick-cli'){
+								return(
+									<li
+										key={index}
+										className = "cli__project"
+									>
+										{element.repositoryName} <a className = "cli__link" href={element.repositoryURL} target="_blank" rel="noopener noreferrer">&lt;repo&gt;</a>  &lt;this webapp&gt;
+									</li>
+								);
+							}else{
+								return(
+									<li
+										key={index}
+										className = "cli__project"
+									>
+										{element.repositoryName} <a className = "cli__link" href={element.repositoryURL} target="_blank" rel="noopener noreferrer">&lt;repo&gt;</a> <a className="cli__link" href={element.repositoryDemoURL} target="_blank" rel="noopener noreferrer">&lt;live&gt;</a>
+									</li>
+								);
+							}
 						}else{
 							return(
 								<li
 									key={index}
 									className = "cli__project"
 								>
-									{element.projectName} <a className = "cli__link" href={`https://github.com/pepeyen/${element.projectName}`} target="_blank" rel="noopener noreferrer">&lt;repo&gt;</a> <a className="cli__link" href={element.projectLiveURL} target="_blank" rel="noopener noreferrer">&lt;live&gt;</a>
+									{element.projectName} <a className = "cli__link" href={`https://github.com/pepeyen/${element.projectName}`} target="_blank" rel="noopener noreferrer">&lt;repo&gt;</a> <span className="deactivated">&lt;live&gt;</span>
 								</li>
 							);
 						}
-					}else{
-						return(
-							<li
-								key={index}
-								className = "cli__project"
-							>
-								{element.projectName} <a className = "cli__link" href={`https://github.com/pepeyen/${element.projectName}`} target="_blank" rel="noopener noreferrer">&lt;repo&gt;</a> <span className="deactivated">&lt;live&gt;</span>
-							</li>
-						);
-					}
-				})
+					})
+				:
+				null
 			}
         </ul>
       </React.Fragment>
